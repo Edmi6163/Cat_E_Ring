83d07709a89f2c734b33614b22aded06ca762d60
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-07-12 16:42:37 +0200

    re-adding classes deleted

---
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/summaryDocument/SummaryDocument.java
@@ -0,0 +1,71 @@
+import java.util.ArrayList;
+
+public class SummaryDocument {
+  
+  public String title; 
+  public String shiftWork;
+  public String menu;
+  public String recipe;
+  public boolean advancedPreparation;
+  public Integer quantityForAdvancedPreparation;
+  public ArrayList<SummaryDocument> listSummaryDocument;
+
+  public SummaryDocument(String title, String shiftWork, String menu, String recipe, boolean advancedPreparation, Integer quantityForAdvancedPreparation) {
+    this.title = title;
+    this.shiftWork = shiftWork;
+    this.menu = menu;
+    this.recipe = recipe;
+    this.advancedPreparation = advancedPreparation;
+    this.quantityForAdvancedPreparation = quantityForAdvancedPreparation;
+    this.listSummaryDocument = new ArrayList<>();
+  }
+
+  public static SummaryDocument createSummaryDocument(String title) {
+    return new SummaryDocument(title, null, null, null, false, null);
+  }
+
+  public void modifieSummaryDocumentTitle(String newTitle) {
+    this.title = newTitle;
+  }
+
+  public void addContext(WorkShift workshift, Recipe recipe, Task task) {
+    if (workshift != null) {
+      this.shiftWork = workshift.toString(); 
+    }
+    if (recipe != null) {
+      this.recipe = recipe.toString(); 
+    }
+  }
+
+  public void modifieContext(SummaryDocument summaryDocument) {
+  }
+
+  public void deleteSummaryDocument(SummaryDocument summaryDocument) {
+    listSummaryDocument.remove(summaryDocument);
+  }
+
+  public SummaryDocument copyDocument() {
+    return new SummaryDocument(this.title, this.shiftWork, this.menu, this.recipe, this.advancedPreparation, this.quantityForAdvancedPreparation);
+  }
+
+  public void moreSummaryDocument() {
+  }
+
+  public SummaryDocument getSummaryDocument() {
+    return this;
+  }
+
+  public void addTask(Task task) {
+  }
+
+  public void assignTaskTo() {
+  }
+
+  public void add(SummaryDocument summaryDocument) {
+    listSummaryDocument.add(summaryDocument);
+  }
+
+  public void remove(SummaryDocument summaryDocument) {
+    listSummaryDocument.remove(summaryDocument);
+  }
+}
---
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/task/TaskManager.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class TaskManager {
+	String assignedTo;
+	boolean typeTask; 
+	int difficulty;
+	ShiftWork shiftWork; 
+
+	public TaskManager(String assignedTo, boolean typeTask, int difficulty, ShiftWork shiftWork) {
+		this.assignedTo = assignedTo;
+		this.typeTask = typeTask;
+		this.difficulty = difficulty;
+		this.shiftWork = shiftWork;
+	}
+
+	public static ArrayList<Task> orderTaskByDifficulty(ArrayList<Task> tasks) {
+		Collections.sort(tasks, Comparator.comparingInt(task -> task.difficulty));
+		return tasks;
+	}
+
+	public static ArrayList<Task> orderTaskByPriority(ArrayList<Task> tasks) {
+		Collections.sort(tasks, (task1, task2) -> Boolean.compare(task2.typeTask, task1.typeTask));
+		return tasks;
+	}
+
+	public static ArrayList<Task> orderTaskByTiming(ArrayList<Task> tasks) {
+		Collections.sort(tasks, Comparator.comparing(task -> task.shiftWork)); 
+		return tasks;
+	}
+
+	public static ArrayList<Task> assignTask(boolean typeTask, Employer name, Employer surname, ShiftWork shiftWork, Task task) {
+		ArrayList<Task> tasks = new ArrayList<>();
+		String assignedTo = (name != null ? name.toString() : "") + " " + (surname != null ? surname.toString() : "").trim();
+		task.assignedTo = assignedTo;
+		task.typeTask = typeTask;
+		task.shiftWork = shiftWork;
+		tasks.add(task);
+		return tasks;
+	}
+}
