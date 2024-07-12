d90b52c1f1450eb79dd8a8de07f0b4eaba87b8f5
Francesco Mauro <francesco.mauro@educ.di.unito.it>
2024-07-12 16:36:09 +0200

    fixed java configuration

--- catering_without_maven/java/catering/src/main/java/catering/businesslogic/summaryDocument/SummaryDocumentReceiver.java
+++
@@ -1,7 +0,0 @@
-import businesslogic.task.*;
-public interface SummaryDocumentReceiver {
-	void updateSummaryDocumentCreated(SummaryDocument sd);
-	void updateSummaryDocumentDeleted(SummaryDocument sd);
-	void updateSummaryDocumentModified(SummaryDocument sd);
-	void updateTask(SummaryDocument sd, Task task);
-}
