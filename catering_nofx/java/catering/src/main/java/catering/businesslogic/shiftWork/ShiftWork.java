b7761cff69855316cc70c253589ba4c406fbdaa7
Riccardo Oro <riccardo.oro@edu.unito.it>
2024-07-13 14:34:29 +0200

    update

--- catering_nofx/java/catering/src/main/java/catering/businesslogic/shiftWork/ShiftWork.java
+++ catering_nofx/java/catering/src/main/java/catering/businesslogic/shiftWork/ShiftWork.java
@@ -1,14 +1,18 @@
 package catering.businesslogic.shiftWork;
 
+import catering.businesslogic.task.Task;
+
 import java.sql.Date;
 import java.sql.Time;
+import java.util.ArrayList;
 
 public class ShiftWork {
   public Date startingDate; 
   public Date endingDate;
   public String location;
   public Time lowerBoundInitShift; 
-  public Time upperBoundInitShift; 
+  public Time upperBoundInitShift;
+  private ArrayList<Task>tasks;
 
   public ShiftWork(Date startingDate, Date endingDate, String location, Time lowerBoundInitShift, Time upperBoundInitShift) {
     this.startingDate = startingDate;
@@ -16,5 +20,13 @@ public class ShiftWork {
     this.location = location;
     this.lowerBoundInitShift = lowerBoundInitShift;
     this.upperBoundInitShift = upperBoundInitShift;
+    this.tasks = new ArrayList<>();
   }
+  public void addTask(Task task){
+    tasks.add(task);
+  }
+  public void removeTask(Task task){
+    tasks.remove(task);
+  }
+
 }
